2e03f3b5c8bd09de4f2f2fde02cc7dc9ae2e1b67
Michelle <michelle.lo14@sec.ycis-hk.com>
2020-04-08 10:30:26 +0000

    PythonAnywhere

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
+from flask.ext.session import Session
 from tempfile import mkdtemp
 
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
