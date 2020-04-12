0c48e6e6cf639ecf21ec3a909d310b3df6721604
Michelle <michelle.lo14@sec.ycis-hk.com>
2020-04-13 00:09:48 +0800

    Added styling for phone

--- app/static/hoverAnimations.js
+++ app/static/hoverAnimations.js
@@ -7,37 +7,37 @@ const packageIcon = document.getElementById("package")
 const packageIcon2 = document.getElementById("package2")
 
 // Hover over logo
-logo.onmouseover = function () {
+logo.onmouseover = function() {
     logo.className = "animated pulse infinite"
 }
 
-logo.onmouseleave = function () {
+logo.onmouseleave = function() {
     logo.className = ""
 }
 
 // Hover over package icons
-packageIcon.onmouseover = function () {
+packageIcon.onmouseover = function() {
     packageIcon.className = "package-icon animated pulse infinite"
 }
 
-packageIcon.onmouseleave = function () {
+packageIcon.onmouseleave = function() {
     packageIcon.className = "package-icon"
 }
 
-packageIcon2.onmouseover = function () {
+packageIcon2.onmouseover = function() {
     packageIcon2.className = "package-icon animated pulse infinite"
 }
 
-packageIcon2.onmouseleave = function () {
+packageIcon2.onmouseleave = function() {
     packageIcon2.className = "package-icon"
 }
 
 // Hover over sidebar
 
-sidebar.onmouseover = function () {
+sidebar.onmouseover = function() {
     sidebar.style.backgroundColor = "rgb(43, 41, 40)"
 }
 
-sidebar.onmouseleave = function () {
+sidebar.onmouseleave = function() {
     sidebar.style.backgroundColor = "rgb(50, 47, 46)"
 }
